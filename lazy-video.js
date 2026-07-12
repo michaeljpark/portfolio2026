613b407a388b0f54ad31ffd92ccc23c66fb5a6b4
michaeljpark <138818450+michaeljpark@users.noreply.github.com>
2026-07-12 16:16:39 -0400

    eryg

--- lazy-video.js
+++ lazy-video.js
@@ -8,7 +8,7 @@
    Optional: data-src-mobile / data-poster-mobile (max-width:880px variants).
    Dynamically created videos: window.observeLazyVid(videoEl). */
 (function () {
-  var ASSET_VERSION = '20260710';
+  var ASSET_VERSION = '20260712';
   try {
     var scriptVersion = new URL(document.currentScript.src, window.location.href).searchParams.get('v');
     if (scriptVersion) ASSET_VERSION = scriptVersion;
